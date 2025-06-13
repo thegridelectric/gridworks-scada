18c8bb34a72bed9e6f0f366dd3b0d4f387fb9bcd
Andrew Schweitzer <schweitz72@gmail.com>
2025-06-13 11:58:24 -0400

    Fix test_show_layout.py in CI

--- tests/test_misc/test_show_layout.py
+++ tests/test_misc/test_show_layout.py
@@ -8,12 +8,20 @@ import show_layout
 from actors.config import ScadaSettings
 
 
-def test_show_layout_on_test_layout():
+def test_show_layout_on_test_layout(tmp_path):
+    env_path = tmp_path / ".env"
+    with open(env_path, "w") as f:
+        f.write("SCADA_IS_SIMULATED=true")
     settings = ScadaSettings()
     if uses_tls(settings):
         copy_keys("scada", settings)
     layout_path = Path(__file__).parent.parent / "config" / "hardware-layout.json"
-    errors = show_layout.main(["-l", str(layout_path)])
+    errors = show_layout.main(
+        [
+            "-e", str(env_path),
+            "-l", str(layout_path)
+         ]
+    )
     if errors:
         rich.print(errors)
     assert not errors
