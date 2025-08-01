841ffdc90cba047fd295d0dcd91b0acfa2b7e8bc
Andrew Schweitzer <schweitz72@gmail.com>
2025-08-01 11:20:43 -0400

    tests: minor cleanup

--- tests/actors/test_live.py
+++ tests/actors/test_live.py
@@ -5,13 +5,7 @@ from tests.utils.scada_live_test_helper import ScadaLiveTest
 @pytest.mark.asyncio
 async def test_tree_connect(request: pytest.FixtureRequest) -> None:
     """This is a simple wait for ATN and Scada to connect to each other."""
-    async with ScadaLiveTest(
-        start_child1=True,
-        start_child2=True,
-        start_parent=True,
-        request=request,
-        child_verbose=True,
-    ) as h:
+    async with ScadaLiveTest(start_all=True, request=request) as h:
         child1 = h.child
         link1to2 = child1.links.link(child1.downstream_client)
         link1toAtn = child1.links.link(child1.upstream_client)
@@ -31,13 +25,7 @@ async def test_tree_connect(request: pytest.FixtureRequest) -> None:
 async def test_tree_quiescent(request: pytest.FixtureRequest) -> None:
     """This is a wait for ATN and Scadas to connect and finishing uploading
     the default number of events. """
-    async with ScadaLiveTest(
-        start_child1=True,
-        start_child2=True,
-        start_parent=True,
-        request=request,
-        child_verbose=True,
-    ) as h:
+    async with ScadaLiveTest(start_all=True, request=request) as h:
         await h.await_quiescent_connections()
 
 
