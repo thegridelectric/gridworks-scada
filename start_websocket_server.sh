f9c80cb98ba7a3ea88a61c441cec2e7b4ddaa081
thdfw <th.defauw@gmail.com>
2025-09-30 15:21:40 +0200

    Simplifying start websocket server script

--- start_websocket_server.sh
+++ start_websocket_server.sh
@@ -2,14 +2,12 @@
 
 echo "Starting GridWorks SCADA WebSocket Server..."
 
-cd /home/ubuntu/gridworks-scada
-
 if [ -d "gw_spaceheat/venv" ]; then
     echo "Activating virtual environment..."
     source gw_spaceheat/venv/bin/activate
 fi
 
 echo "Starting server..."
-python -m gw_spaceheat.webinter.cli --host 0.0.0.0 --port 8080 --verbose
+python -m /home/ubuntu/gridworks-scada/gw_spaceheat.webinter.cli --verbose
 
 echo "Server stopped."
