ca377fa976f35f902f645d86d35dd72e70d31a4b
thdfw <th.defauw@gmail.com>
2025-12-12 18:07:39 +0100

    Passing tests

--- tests/enums/home_alone_top_state_test.py
+++ tests/enums/home_alone_top_state_test.py
@@ -16,4 +16,4 @@ def test_local_control_top_state() -> None:
 
     assert LocalControlTopState.default() == LocalControlTopState.Dormant
     assert LocalControlTopState.enum_name() == "local.control.top.state.neb"
-    assert LocalControlTopState.enum_version() == "001"
+    assert LocalControlTopState.enum_version() == "000"
