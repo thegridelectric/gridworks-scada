74df977e86f87f5e7e7071b7a31c068c5c3ca4ec
thdfw <th.defauw@gmail.com>
2026-06-16 09:51:27 +0200

    Fixed changing modes bug

--- gw_spaceheat/actors/sieg_loop_loader.py
+++ gw_spaceheat/actors/sieg_loop_loader.py
@@ -145,7 +145,12 @@ class SiegLoop(ShNodeActor):
             if hasattr(old_impl, "valve_state") and hasattr(new_impl, "valve_state"):
                 old_valve_state = getattr(old_impl, "valve_state")
                 new_valve_state_type = type(getattr(new_impl, "valve_state"))
-                setattr(new_impl, "valve_state", new_valve_state_type(old_valve_state.value))
+                raw = (
+                    old_valve_state
+                    if not hasattr(old_valve_state, "value")
+                    else old_valve_state.value
+                )
+                setattr(new_impl, "valve_state", new_valve_state_type(raw))
 
         self._impl = new_impl
         self._mode = mode
